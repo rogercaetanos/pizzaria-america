be2133c9d9b9a5ebc1775244f4b1ceb83921bf0f
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-05-20 11:38:40 -0300

    anexado o projeto back-end

---
+++ src/back-end/pizzarianapoli/src/main/java/com/itb/in2em/pizzarianapoli/PizzarianapoliApplication.java
@@ -0,0 +1,17 @@
+package com.itb.in2em.pizzarianapoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzarianapoliApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(PizzarianapoliApplication.class, args);
+
+        System.out.println("PizzarianapoliApplication started port 8080");
+
+	}
+
+}
---
+++ src/back-end/pizzarianapoli/src/test/java/com/itb/in2em/pizzarianapoli/PizzarianapoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.in2em.pizzarianapoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzarianapoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
