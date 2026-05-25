a5e05e2bb8ca6cf9c3356bec05fc54679ee93b10
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-05-25 08:26:33 -0300

    BackEnd: Camada Model Entity em Construção

---
+++ src/back-end/pizzarianapoli/src/main/java/com/itb/in2em/pizzarianapoli/model/entity/Usuario.java
@@ -0,0 +1,122 @@
+package com.itb.in2em.pizzarianapoli.model.entity;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+    private String sexo;
+    private String logradouro;
+    private String cep;
+    private String bairro;
+    private String cidade;
+    private boolean codStatus;
+    private String tipoUsuario; // Admin, Clinte ou Funcionario
+    private String uf;
+
+    // Setter´s | Getter´s
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public boolean isCodStatus() {
+        return codStatus;
+    }
+
+    public void setCodStatus(boolean codStatus) {
+        this.codStatus = codStatus;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    
+
+}
